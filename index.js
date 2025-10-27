dc78e3e93a9ba73d0fb6276d5f95cd2e66a6b81a
SarbBhangu <Bhangu513@gmail.com>
2025-10-27 12:23:19 -0400

    Added items to the shooping list using push

---
+++ index.js
@@ -0,0 +1,8 @@
+let shoppingList= []
+shoppingList.push("Apple");
+shoppingList.push("Soda");
+shoppingList.push("TV");
+shoppingList.push("Milk");
+shoppingList.push("Water");
+
+console.log(shoppingList);
